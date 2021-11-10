74d4d78e2460e1fb51869b81b5004b0b22f32809
andreipastrav <82665499+AndreiEU@users.noreply.github.com>
2021-11-10 10:42:17 +0200

    Add files via upload

---
+++ EcuationOfDegree1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EcuationOfDegree1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a,b,x;
+            Console.WriteLine("Number a = ");
+            a = float.Parse(Console.ReadLine());
+            Console.WriteLine("Number b = ");
+            b = float.Parse(Console.ReadLine());
+            if(a == 0)
+            {
+                if(b == 0)
+                {
+                    Console.WriteLine("Ecuation unlimited");
+                }
+                else
+                {
+                    Console.WriteLine("Ecuation impossible");
+                }
+            }
+                else
+                {
+                    x = -b/a;
+                    Console.WriteLine("The result of ecuation {0}",x);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+
