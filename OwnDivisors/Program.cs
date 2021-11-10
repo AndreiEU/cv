3042a0bbd55f608e59d0269ce9518cac2b0c1987
andreipastrav <82665499+AndreiEU@users.noreply.github.com>
2021-11-10 10:41:24 +0200

    Add files via upload

---
+++ OwnDivisors/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OwnDivisors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sem = 0;
+            Console.WriteLine("Enter a number");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("The numbers divisors are:");
+            for(int i = 2; i<=n/2 ; i++)
+            {
+                if(n % i == 0)
+                {
+                    Console.WriteLine(i);
+                    sem=1;
+                }
+                if(sem==0)
+                Console.WriteLine("There are not divisors");
+            }
+        }
+    }
+}
