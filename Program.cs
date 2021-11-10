b21999c30c68c24ebbdb4a1d488339b88b1836f2
andreipastrav <82665499+AndreiEU@users.noreply.github.com>
2021-11-10 09:49:51 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RandomNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(0,100);
+            bool sal = false;
+            while(!sal)
+            {
+                sal = true;
+                Console.WriteLine("Guess the number");
+                int z = int.Parse(Console.ReadLine());
+                sal = false;
+                if(z > a)
+                {
+                    Console.WriteLine("Write a smaller number :");
+                }
+
+                if(z < a)
+                {
+                    Console.WriteLine("Write a greater number:");
+                }
+
+                if(z == a)
+                {
+                    Console.WriteLine("Congratulations ! you guessed the number :D");
+                    break;
+                }
+            }
+        }
+    }
+}
