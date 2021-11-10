8ca6d9547173bb805b98e64e054210da02bdee30
andreipastrav <82665499+AndreiEU@users.noreply.github.com>
2021-11-10 10:10:04 +0200

    Add files via upload

---
+++ GreatestCommonFactor/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GreatestCommonFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write first number:");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write second number:");
+            int b = int.Parse(Console.ReadLine());
+
+            int r = a % b;
+            while (r != 0)
+            {
+                a = b;
+                b = r;
+                r = a % b;
+            }
+            Console.WriteLine("GCF is {0}", b);
+        }
+    }
+}
