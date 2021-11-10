6cc98ec30dae035f54a8b6b39cbcdede876f5f1a
andreipastrav <82665499+AndreiEU@users.noreply.github.com>
2021-11-10 10:59:02 +0200

    Add files via upload

---
+++ Interchange/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Interchange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float a, b, x;
+            Console.Write("Enter a = ");
+            a = float.Parse(Console.ReadLine());
+            Console.Write("Enter b = ");
+            b = float.Parse(Console.ReadLine());
+            if(a == b)
+               if(b == 0)
+               Console.WriteLine("Ecuation nedet.");
+               else 
+                  Console.WriteLine("Ecuation impossible");
+              else
+              {
+                  x = -b / a;
+                  Console.WriteLine("Solution is x = {0}", x);
+              }
+              Console.ReadKey();
+                
+        }
+    }
+}
