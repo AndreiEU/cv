052cab2fde2fe67564d1c537b170df8f18977f66
andreipastrav <82665499+AndreiEU@users.noreply.github.com>
2021-11-10 10:32:47 +0200

    Add files via upload

---
+++ Divisibility/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Divisibility
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a,b;
+            Console.WriteLine("Write first number");
+            a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Write second number");
+            b = int.Parse(Console.ReadLine());
+            if(a % b == 0 || b % a == 0)
+            {
+                Console.WriteLine("are divisible");
+            }
+            else
+            {
+                Console.WriteLine("they are not divisible");
+            }
+        }
+    }
+}
